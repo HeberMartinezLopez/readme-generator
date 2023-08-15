1f5694b3fedc96d394de08d3fc13d0954c3842c7
Heber Martinez Lopez <hebermartinez14@gmail.com>
2023-08-15 11:55:46 -0700

    work on license links

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,8 +2,8 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   const badge = '';
-  if(license = true){ 
-    
+  if(license !== 'None'){ 
+    return `![GitHub license](https://img.shields.io/github/license-${license}-blue.svg)`;
   }else{
     
     return badge;
@@ -12,19 +12,38 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const link = '';
+  if(license !== 'None'){ 
+    return `*[License](#license)`;
+  }else{
+    
+    return link;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const section = '';
+  if(license !== 'None'){ 
+    return `## License
+    Licensed under ${license} license.
+    `;
+  }else{
+    
+    return section;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
   ## Table of Contents
   *[Description](#description)
   *[Installation](#installation)
-  *[License](#license)
+  ${renderLicenseLink(data.license)}
   *[Usage](#usage)
   *[Contributing](#contributing)
   *[Tests](#tests)
@@ -52,6 +71,7 @@ function generateMarkdown(data) {
   Contact me if you have any questions at ${data.email}.
   Feel free to checkout my GitHub at ${data.gitHub}.
 
+  ${renderLicenseSection(data.license)}
 
 `;
 }
